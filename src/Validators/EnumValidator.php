acda576064ca44b68dd3324ed9d7eddc5dbec416
Abdul Wahab Qureshi <its.inevitable@hotmail.com>
2020-02-26 09:49:04 +0000

    Enum type defined.

--- src/Validators/EnumValidator.php
+++ src/Validators/EnumValidator.php
@@ -9,6 +9,7 @@ class EnumValidator implements Validator
 {
     public static function validate($value, array $details): void
     {
+        Assert::assertArrayHasKey('enum', $details, 'Enum type must declare enum key with values to check for.');
         Assert::assertTrue(
             in_array($value, $details['enum']),
             sprintf('%s value not allowed, allowed values are: ', $value, print_r($details['enum'], true))
